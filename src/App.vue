a6414510f1ac2c5dfc13d2798c4871488b14d303
Karl Heitmann <khf1988@gmail.com>
2022-05-03 23:57:45 -0400

    FIRST REFACTOR, first level of fractals

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <script setup>
-  import { ref, onMounted } from "vue"
+  import { ref } from "vue"
   import axios from 'axios'
+
+  import Navbar from "@/components/Navbar.vue";
+  import Card from "@/components/Card.vue";
+
   const pokemons = ref([])
   const fetchAllPokemons = () => {
     console.log("fetchAllPokemons")
@@ -20,89 +24,26 @@
       console.log(error);
     });
   }
-  const fetchPokemon = (url) => {
-    console.log("fetchPokemon", url)
-    const config = {
-      method: 'get',
-      url,
-      headers: { }
-    };
-
-    axios(config)
-    .then(function (response) {
-      const { data } = response
-      console.log(data)
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-  }
 </script>
 
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><hr class="dropdown-divider"></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled">Disabled</a>
-          </li>
-        </ul>
-        <form class="d-flex">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
-      </div>
-    </div>
-  </nav>
-  <button
-    class="btn btn-primary"
-    @click="fetchAllPokemons"
-  >
-    Fetch all data
-  </button>
-  <div class="row">
-    <div
-        v-for="pokemon in pokemons"
-      class="col-3"
-      >
+  <Navbar />
+  <div class="container">
+    <button
+      class="btn btn-primary"
+      @click="fetchAllPokemons"
+    >
+      Fetch all data
+    </button>
+    <div class="row">
       <div
-        class="card">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-          <button
-            class="btn btn-primary"
-            @click="fetchPokemon(pokemon.url)"
-          >
-            {{ pokemon.name }}
-          </button>
-          <!--
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-          -->
-        </div>
+        class="col-3"
+        v-for="pokemon in pokemons"
+        :key="pokemon.url"
+      >
+        <Card
+          :pokemon="pokemon"
+        />
       </div>
     </div>
   </div>
@@ -112,62 +53,4 @@
 @import './assets/base.css';
 @import './assets/bootstrap.min.css';
 
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
-}
-
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-}
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,47 @@
+<script setup>
+  import axios from 'axios'
+  // const pokemon = {name: "placeholder", url: "#"}
+  const props = defineProps({
+    pokemon: {
+      type: Object,
+      required: false
+    }
+  })
+  const { pokemon } = props
+  const fetchPokemon = (url) => {
+    console.log("fetchPokemon", url)
+    const config = {
+      method: 'get',
+      url,
+      headers: { }
+    };
+
+    axios(config)
+    .then(function (response) {
+      const { data } = response
+      console.log(data)
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+</script>
+<template>
+  <div
+    class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <button
+        class="btn btn-primary"
+        @click="fetchPokemon(pokemon.url)"
+      >
+        {{ pokemon.name }}
+      </button>
+      <!--
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+      -->
+    </div>
+  </div>
+</template>
