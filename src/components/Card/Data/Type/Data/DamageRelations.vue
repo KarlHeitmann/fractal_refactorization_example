3919b1ac8f73a5cd2ca8a968b6066f8fac449938
Karl Heitmann <khf1988@gmail.com>
2022-05-07 22:25:07 -0400

    Added fractal DamageRelations

---
+++ src/components/Card/Data/Type/Data/DamageRelations.vue
@@ -0,0 +1,72 @@
+<script setup>
+  import Type from '../index.vue'
+
+  const props = defineProps({
+    damage_relations: {
+      type: Object,
+      required: true
+    }
+  })
+
+  const {damage_relations} = props
+  console.log("DamageRelations", props)
+</script>
+<template>
+  <p>Damage Relations</p>
+  <ul>
+    <li>
+      Double damage from
+      <ul>
+        <Type
+          v-for="type_nested in damage_relations.double_damage_from"
+          :type="type_nested"
+        />
+      </ul>
+    </li>
+    <li>
+      Double damage to
+      <ul>
+        <Type
+          v-for="type_nested in damage_relations.double_damage_to"
+          :type="type_nested"
+        />
+      </ul>
+    </li>
+    <li>
+      Half damage from
+      <ul>
+        <Type
+          v-for="type_nested in damage_relations.half_damage_from"
+          :type="type_nested"
+        />
+      </ul>
+    </li>
+    <li>
+      Half damage to
+      <ul>
+        <Type
+          v-for="type_nested in damage_relations.half_damage_to"
+          :type="type_nested"
+        />
+      </ul>
+    </li>
+    <li>
+      No damage from
+      <ul>
+        <Type
+          v-for="type_nested in damage_relations.no_damage_from"
+          :type="type_nested"
+        />
+      </ul>
+    </li>
+    <li>
+      No damage to
+      <ul>
+        <Type
+          v-for="type_nested in damage_relations.no_damage_to"
+          :type="type_nested"
+        />
+      </ul>
+    </li>
+  </ul>
+</template>
--- src/components/Card/Data/Type/Data/index.vue
+++ src/components/Card/Data/Type/Data/index.vue
@@ -1,6 +1,6 @@
 <script setup>
   import {onMounted, ref} from 'vue'
-  import Type from '../index.vue'
+  import DamageRelations from './DamageRelations.vue'
 
   const props = defineProps({
     type: {
@@ -21,62 +21,8 @@
 <template>
   <li>
     <h5>{{ type.name }}</h5>
-    <p>Damage Relations</p>
-    <ul>
-      <li>
-        Double damage from
-        <ul>
-          <Type
-            v-for="type_nested in type.damage_relations.double_damage_from"
-            :type="type_nested"
-          />
-        </ul>
-      </li>
-      <li>
-        Double damage to
-        <ul>
-          <Type
-            v-for="type_nested in type.damage_relations.double_damage_to"
-            :type="type_nested"
-          />
-        </ul>
-      </li>
-      <li>
-        Half damage from
-        <ul>
-          <Type
-            v-for="type_nested in type.damage_relations.half_damage_from"
-            :type="type_nested"
-          />
-        </ul>
-      </li>
-      <li>
-        Half damage to
-        <ul>
-          <Type
-            v-for="type_nested in type.damage_relations.half_damage_to"
-            :type="type_nested"
-          />
-        </ul>
-      </li>
-      <li>
-        No damage from
-        <ul>
-          <Type
-            v-for="type_nested in type.damage_relations.no_damage_from"
-            :type="type_nested"
-          />
-        </ul>
-      </li>
-      <li>
-        No damage to
-        <ul>
-          <Type
-            v-for="type_nested in type.damage_relations.no_damage_to"
-            :type="type_nested"
-          />
-        </ul>
-      </li>
-    </ul>
+    <DamageRelations
+      :damage_relations="type.damage_relations"
+    />
   </li>
 </template>
