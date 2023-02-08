41fa46991e6a6668eeed0b42c0889d25154d1b64
Karl Heitmann <khf1988@gmail.com>
2023-02-07 23:15:33 -0300

    Created DamageRelations component

---
+++ src/components/DamageRelations.vue
@@ -0,0 +1,78 @@
+<script setup>
+  const props = defineProps({
+    type_data: {
+      type: Object,
+      required: true,
+    },
+  })
+  // console.log("======= type data: ", type_data)
+  const t = props.type_data.type
+  // console.log("//////////////////\\\\\\\\\\\\\\\\", props.type_data.type)
+  console.log("//////////////////\\\\\\\\\\\\\\\\", t)
+</script>
+<template>
+  <h5>{{ t.name }}</h5>
+  <p>Damage Relations</p>
+  <ul>
+    <li>
+      Double damage from
+      <ul>
+        <li
+          v-for="type_nested in t.damage_relations.double_damage_from"
+        >
+          {{type_nested.name}}
+        </li>
+      </ul>
+    </li>
+    <li>
+      Double damage to
+      <ul>
+        <li
+          v-for="type_nested in t.damage_relations.double_damage_to"
+        >
+          {{type_nested.name}}
+        </li>
+      </ul>
+    </li>
+    <li>
+      Half damage from
+      <ul>
+        <li
+          v-for="type_nested in t.damage_relations.half_damage_from"
+        >
+          {{type_nested.name}}
+        </li>
+      </ul>
+    </li>
+    <li>
+      Half damage to
+      <ul>
+        <li
+          v-for="type_nested in t.damage_relations.half_damage_to"
+        >
+          {{type_nested.name}}
+        </li>
+      </ul>
+    </li>
+    <li>
+      No damage from
+      <ul>
+        <li
+          v-for="type_nested in t.damage_relations.no_damage_from"
+        >
+          {{type_nested.name}}
+        </li>
+      </ul>
+    </li>
+    <li>
+      No damage to
+      <ul>
+        <li
+          v-for="type_nested in t.damage_relations.no_damage_to"
+        >
+          {{type_nested.name}}
+        </li>
+      </ul>
+    </li>
+  </ul>
+</template>
--- src/components/Type.vue
+++ src/components/Type.vue
@@ -1,6 +1,7 @@
 <script setup>
   import axios from 'axios'
   import { reactive, ref } from 'vue'
+  import DamageRelations from './DamageRelations.vue'
 
   const stateOn = ref(false)
   const type_data = reactive({})
@@ -38,70 +39,9 @@
   <li
     v-if="stateOn"
     >
-    <h5>{{ type_data.name }}</h5>
-    <p>Damage Relations</p>
-    <ul>
-      <li>
-        Double damage from
-        <ul>
-          <li
-            v-for="type_nested in type_data.type.damage_relations.double_damage_from"
-          >
-            {{type_nested.name}}
-          </li>
-        </ul>
-      </li>
-      <li>
-        Double damage to
-        <ul>
-          <li
-            v-for="type_nested in type_data.type.damage_relations.double_damage_to"
-          >
-            {{type_nested.name}}
-          </li>
-        </ul>
-      </li>
-      <li>
-        Half damage from
-        <ul>
-          <li
-            v-for="type_nested in type_data.type.damage_relations.half_damage_from"
-          >
-            {{type_nested.name}}
-          </li>
-        </ul>
-      </li>
-      <li>
-        Half damage to
-        <ul>
-          <li
-            v-for="type_nested in type_data.type.damage_relations.half_damage_to"
-          >
-            {{type_nested.name}}
-          </li>
-        </ul>
-      </li>
-      <li>
-        No damage from
-        <ul>
-          <li
-            v-for="type_nested in type_data.type.damage_relations.no_damage_from"
-          >
-            {{type_nested.name}}
-          </li>
-        </ul>
-      </li>
-      <li>
-        No damage to
-        <ul>
-          <li
-            v-for="type_nested in type_data.type.damage_relations.no_damage_to"
-          >
-            {{type_nested.name}}
-          </li>
-        </ul>
-      </li>
-    </ul>
+    <DamageRelations
+      :type_data="type_data"
+    />
   </li>
   <li
     v-else
